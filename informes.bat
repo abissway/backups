c7bdc756bf965c552754810bc81f3a4307aa964c
abissway <abissway@gmail.com>
2017-01-13 08:32:58 +0100

    informes.bat
    Script útil para la copia de plantillas de reports y ejecutables en Informes e imagen en sus versiones 2015 y anteriores.

---
+++ informes.bat
@@ -0,0 +1,13 @@
+@echo off 
+color 0E 
+title BIENVENIDO AL PROGRAMA 
+:menuprincipal
+echo. 
+cls 
+Xcopy  "\\192.168.1.6\rx\AUTO\correccions\Informes" "C:\AUTO\informes" /Y
+Xcopy  "\\192.168.1.6\rx\AUTO\correccions\imagen" "C:\AUTO\imagen" /Y
+Xcopy  "\\192.168.1.6\rx\AUTO\correccions\Informes2015" "C:\AUTO\informes2015" /Y
+Xcopy  "\\192.168.1.6\rx\AUTO\correccions\imagen2015" "C:\AUTO\imagen2015" /Y
+
+echo.
+pause
